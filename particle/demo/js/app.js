a3f6b93e6b857d0d38730319ba2a811e1004d715
sxy135 <sunxiaoyu135@hotmail.com>
2020-01-05 01:21:39 -0500

    homepage+mg update

--- particle/demo/js/app.js
+++ particle/demo/js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 66,
+        "value": 56,
         "density": {
           "enable": true,
           "value_area": 800
