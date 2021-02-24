ae5513439df6c99e726f056f802a6ad319418330
sxy135 <sunxiaoyu135@hotmail.com>
2021-02-24 15:42:00 -0500

    kma update 0224

--- loginScript.js
+++ loginScript.js
@@ -8,3 +8,14 @@ function checkPswd() {
               alert("Passwords do not match.");
           }
       }
+
+function checkPswd2() {
+          var confirmPassword = "KMA 2019";
+          var password = document.getElementById("pswd2").value;
+          if (password == confirmPassword) {
+              window.location.href="kmawithpsw.html";
+          }
+          else{
+              alert("Passwords do not match.");
+          }
+      }
